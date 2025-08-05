51c93d900eea5a5630be3205f4474a945717a0f4
dotmagic-infotech <info@dotmagicinfotech.com>
2025-08-05 12:56:41 +0530

    add build

--- dist/assets/AccessDenied-DvMPnt_O.js
+++
@@ -1,1 +0,0 @@
-import{j as e,B as t,T as s}from"./index-CVsBOibh.js";const n=()=>e.jsxs(t,{style:{padding:"1.25rem",height:"100%",gap:"0.5rem",display:"flex",justifyContent:"center",alignItems:"center",flexDirection:"column"},children:[e.jsx(s,{variant:"h4",children:"🚫 Access Denied"}),e.jsx(s,{children:"You do not have permission to view this page."})]});export{n as default};
